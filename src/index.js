31a9debf7d22c90a14f12a43fb33155cb9ae1540
Zhongkang Shen <szk1033743385@gmail.com>
2022-04-04 22:31:33 +0800

    add css, sass, postcss, hotreloading, browserslist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './styles/index.scss';
+
 const elvenShieldRecipe = {
   leatherStrips: 2,
   ironIngot: 1,
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 let mode = 'development';
+let target = 'web';
 
 if (process.env.NODE_ENV === 'production') {
   mode = 'production';
+  target = 'browserslist';
 }
 
 module.exports = {
   mode,
+  target,
   module: {
     rules: [
       {
@@ -16,13 +20,26 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ]      
       }
     ]
   },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ],
   devtool: 'source-map',
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist')
-    }
+    },
+    hot: true
   }
 };
